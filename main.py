e0d1105708f13aba4178c318462d0a4a366233a5
jack-99yinfa <victorrr1109@gmail.com>
2026-05-13 18:50:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Bot iniciado")
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot conectado como {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong! El bot está vivo")
+
+bot.run(TOKEN)
