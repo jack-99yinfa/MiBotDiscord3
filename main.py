b38860b0d02ae71b1ceeed7bafdc4a7d18ce8595
jack-99yinfa <victorrr1109@gmail.com>
2026-05-13 19:15:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="apl ", intents=intents)
 
 @bot.event
 async def on_ready():
